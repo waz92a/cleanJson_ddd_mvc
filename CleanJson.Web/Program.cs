ac428d7e6b16f38eef1f41ddca06671474376f04
Senior Dev <26078620+universaldev22@users.noreply.github.com>
2025-08-13 01:28:44 +0100

    Revert "Force HTTP/1.1 for remote JSON fetch"

--- CleanJson.Web/Program.cs
+++ CleanJson.Web/Program.cs
@@ -4,9 +4,6 @@ using CleanJson.Infrastructure.Cleaning;
 using CleanJson.Infrastructure.Http;
 using CleanJson.Infrastructure.Options;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Net;
-using System.Net.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,21 +28,7 @@ builder.Services.Configure<RemoteJsonOptions>(
     builder.Configuration.GetSection(RemoteJsonOptions.SectionName));
 
 // DI (DDD)
-builder.Services.AddHttpClient<IRemoteJsonSource, RemoteJsonSource>(client =>
-{
-    // Some third-party APIs do not fully support HTTP/2 and may terminate
-    // the response early. Force HTTP/1.1 to avoid "response ended prematurely"
-    // errors when fetching remote JSON content.
-    client.DefaultRequestVersion = new Version(1, 1);
-    client.DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrLower;
-})
-.ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
-{
-    // Completely disable HTTP/2 at the handler level and ensure the
-    // response stream is decompressed before deserialization.
-    AutomaticDecompression = DecompressionMethods.All,
-    MaxVersion = HttpVersion.Version11,
-});
+builder.Services.AddHttpClient<IRemoteJsonSource, RemoteJsonSource>();
 builder.Services.AddSingleton<IJsonCleaner, NewtonsoftJsonCleaner>();
 builder.Services.AddScoped<CleanRemoteJsonHandler>();
 
